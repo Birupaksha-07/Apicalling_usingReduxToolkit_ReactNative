a4d34d632ea357f32927f1ff145cb9f69febd75e
Birupaksha-07 <birupaksha.babu@gmail.com>
2023-09-26 11:34:37 +0530

    updated

--- src/navigation/AppNav.js
+++ src/navigation/AppNav.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, ScrollView, FlatList , Image } from 'react-native'
+import { View, Text, StyleSheet, Button, ScrollView, FlatList, Image } from 'react-native'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux';
@@ -7,28 +7,67 @@ import { fetchProducts } from '../redux/slice/productSlice';
 const AppNav = () => {
 
     const dispatch = useDispatch();
-    const products = useSelector((state) => state.product.data)
-    console.log("Products Are -->", products);
-
-    const data1 = [{ name: 'biru' }, { name: 'ankan' }, { name: 'soheli' }]
+    const response = useSelector((state) => state);
+    const isLoading = response.product.isLoader;
+    const products = response.product.data;
+    // console.log("Products Are -->", products);
 
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 20, marginBottom: 10 }}>Calling Api....</Text>
-            <Button title='Call Api' onPress={() => dispatch(fetchProducts())} />
-            <ScrollView>
+        <View style={{ flex: 1, }}>
+            <View style={{ flex:1,justifyContent: 'center', alignItems: 'center',marginTop:50 }}>
+                <Text style={{fontSize: 20, marginBottom: 10 }}>Calling Api....</Text>
+                <Button title='Call Api' onPress={() => dispatch(fetchProducts())} />
+            </View>
 
-                {products.map((item) => {
-                    return(
-                        <View key={item.id}>
-                        <Text>{item.title}</Text>
-                        </View>
-                    )
-                })}
-            </ScrollView>
+            <View style={{height: 800, marginTop: 20, padding: 20 }}>
+                {isLoading ? <Text style={{ fontSize: 20 , textAlign:'center', marginBottom:20}}>Loading...</Text> : ""}
+
+                <ScrollView>
+                    {
+                        products == null ? "" :
+                            products.map((item) => {
+                                return (
+                                    <View key={item.id} style={styles.productBox}>
+                                        <Text style={styles.productTextId}>{item.id}</Text>
+                                        <Text style={styles.productText}>{item.title.substring(0,50)+"..."}</Text>
+                                        <Text style={styles.productText}>{item.price}</Text>
+                                        <Text style={styles.productText}> {item.description.substring(0,100)+"..."}</Text>
+                                    </View>
+                                )
+                            })
+                    }
+                </ScrollView>
+            </View>
+
+
         </View>
     )
 }
+const styles = StyleSheet.create({
+productBox:{
+    height:180,
+    backgroundColor:'#f0f0f0',
+    borderRadius:12,
+    padding:12,
+    marginBottom:12,
+},
+productTextId:{
+    backgroundColor:'lightblue',
+    width:80,
+    textAlign:'center',
+    borderRadius:7,
+    fontSize:20,
+    fontWeight:'bold'
+},
+productText:{
+    marginTop:5,
+    backgroundColor:'#fff',
+    borderRadius:8,
+    padding:5,
+    elevation:8,
+}
+});
 
 export default AppNav;
+
